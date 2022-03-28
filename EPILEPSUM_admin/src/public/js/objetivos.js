a972d7420d1f395772c632876961f61aabb6c165
ema-munoz <ema.munoz@yavirac.edu.ec>
2022-03-28 15:36:23 -0500

    28/03/2022

---
+++ EPILEPSUM_admin/src/public/js/objetivos.js
@@ -0,0 +1,16 @@
+class objetivo{
+    constructor(){
+        this.contenedor = document.getElementById('objetivos')
+        this.numero = document.getElementById('numeros')
+    }
+    crear(){
+        for(let i = 0; i<parseInt(this.numero.value); i++){
+        let input = document.createElement('input')
+        input.setAttribute('type', 'text')
+        input.name = 'objetivos'
+        this.contenedor.appendChild(input)
+        }
+    }
+}
+
+let objetivos = new objetivo
