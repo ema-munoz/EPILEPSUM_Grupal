0767199b4bba8571c00e8e72942f004922832f89
User <msp.calero@yavirac.edu.ec>
2022-04-24 21:30:10 -0500

    nuevos cambios de hoy

--- EPILEPSUM_admin/src/controllers/councils.controller.js
+++ EPILEPSUM_admin/src/controllers/councils.controller.js
@@ -2,35 +2,32 @@ const councilsCtl = {};
 
 const baseDatosSQL = require("../Configuration/basededatos.sql");
 
-
 councilsCtl.getScreeam = async (req, res) => {
-
-  const councils = await baseDatosSQL.query('SELECT *FROM consejos');
+  const councils = await baseDatosSQL.query("SELECT *FROM consejos");
 
   //    console.log(users);
-  res.render('pages/councils/councils-list', { councils });
+  res.render("pages/councils/councils-list", { councils });
 };
 
 councilsCtl.add = async (req, res) => {
   const { nombreConsejo, descripcionConsejo } = req.body;
   const newCouncil = {
     nombreConsejo,
-    descripcionConsejo
-    
+    descripcionConsejo,
   };
   //console.log(newCouncil);
-  await baseDatosSQL.query('INSERT INTO consejos set ?', [newCouncil]);
-  req.flash('success', 'se agrego un consejo');
-  res.redirect('/list-councils');
+  await baseDatosSQL.query("INSERT INTO consejos set ?", [newCouncil]);
+  req.flash("success", "se agrego un consejo");
+  res.redirect("/list-councils");
 };
 
 councilsCtl.delete = async (req, res) => {
   const { id } = req.params;
-  await baseDatosSQL.query('DELETE FROM consejos WHERE ID = ?', [id]);
+  await baseDatosSQL.query("DELETE FROM consejos WHERE ID = ?", [id]);
 
-  req.flash('success', 'Experiencia borrado correctamente');
+  req.flash("success", "Experiencia borrado correctamente");
 
-  res.redirect('/list-councils');
+  res.redirect("/list-councils");
 };
 
 module.exports = councilsCtl;
