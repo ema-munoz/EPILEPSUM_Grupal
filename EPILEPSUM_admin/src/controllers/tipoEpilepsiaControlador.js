fa50d097e61a4da35108e8c7e140d908adbf7775
ema-munoz <ema.munoz@yavirac.edu.ec>
2022-04-25 23:52:12 -0500

    Cambios de Ever 25/04/2022.

--- EPILEPSUM_admin/src/controllers/tipoEpilepsiaControlador.js
+++ EPILEPSUM_admin/src/controllers/tipoEpilepsiaControlador.js
@@ -3,76 +3,58 @@ const tipoEpilepsiaCtl = {}
 const baseDatosSQL = require("../Configuration/basededatos.sql");
 const baseDatosORM = require("../Configuration/basededatos.orm");
 
-tipoEpilepsiaCtl.mostrar = async (req, res) => {
-    res.render('tipoEpilepsia/tipoEpilepsiaAgragar')
+tipoEpilepsiaCtl.mostrar = async(req, res) => {
+    res.render('tipoEpilepsia/tipoEpilepsiaAgragar' )
 }
 
-tipoEpilepsiaCtl.enviar = async (req, res) => {
-    const id = req.user.idusuario
-    const {
-        nombreTipoEpilepsia,
-        descripcionTipoEpilepsia
-    } = req.body
+tipoEpilepsiaCtl.enviar = async(req, res) => {
+    const id = req.user.idUsuario
+    const {  nombreTipoEpilepsia, descripcionTipoEpilepsia} = req.body
     const nuevoEnvio = {
         nombreTipoEpilepsia,
         descripcionTipoEpilepsia,
-        usuarioIdusuario: id
+        usuarioIdusuario: id  
     }
     await baseDatosORM.tipoEpilepsia.create(nuevoEnvio)
     req.flash('success', 'guardado')
     res.redirect('/tipoEpilepsia/lista/' + id)
+    }
+
+
+    tipoEpilepsiaCtl.lista = async(req, res) => {
+const ids = req.params.id
+const lista = await baseDatosSQL.query('select * from  TipoEpilepsia')
+res.render('tipoEpilepsia/tipoEpilepsiaListas',{ lista})
 }
 
-
-tipoEpilepsiaCtl.lista = async (req, res) => {
+tipoEpilepsiaCtl.traer = async(req, res) => {
     const ids = req.params.id
-    const lista = await baseDatosSQL.query('select * from  TipoEpilepsia')
-    res.render('tipoEpilepsia/tipoEpilepsiaListas', {
-        lista
-    })
-}
-
-tipoEpilepsiaCtl.traer = async (req, res) => {
-    const ids = req.params.id
-    const lista = await baseDatosSQL.query('select * from  tipoEpilepsia where usuarioIdusuario = ?', [ids])
-    res.render('tipoEpilepsia/tipoEpilepsiaListas', {
-        lista
-    })
+    const lista = await  baseDatosSQL.query('select * from  tipoEpilepsia where idTipoEpilepsia = ?', [ids])
+    res.render('tipoEpilepsia/tipoEpilepsiaEditar', { lista})
 
 }
 
-tipoEpilepsiaCtl.actualizar = async (req, res) => {
+tipoEpilepsiaCtl.actualizar = async(req, res) => {
     const ids = req.params.id
-    const id = req.user.idusuario
-    const {
-        nombreTipoEpilepsia,
-        descripcionTipoEpilepsia
-    } = req.body
+    const id = req.user.idUsuario
+    const { nombreTipoEpilepsia,descripcionTipoEpilepsia } = req.body
     const nuevoEnvio = {
         nombreTipoEpilepsia,
         descripcionTipoEpilepsia
-
+    
     }
-    await baseDatosORM.proyecto.findOne({
-            where: {
-                idTipoEpilepsia: ids
-            }
-        })
-        .then(actualizar => {
-            actualizar.update(nuevoEnvio)
-            req.flash("success", "Datos Actulizados")
-            res.redirect('/tipoEpilepsia/lista/' + id)
-        })
+    await baseDatosORM.tipoEpilepsia.findOne({ where: { idTipoEpilepsia: ids } })
+    .then(actualizar => {
+        actualizar.update(nuevoEnvio)
+        req.flash("success","Datos Actulizados")
+        res.redirect('/tipoEpilepsia/lista/' + id)
+    })
 }
 
-tipoEpilepsiaCtl.eliminar = async (req, res) => {
+tipoEpilepsiaCtl.eliminar = async(req, res) => {
     const id = req.params.id
-    const ids = req.user.idusuario
-    await baseDatosORM.tipoEpilepsia.destroy({
-        where: {
-            idTipoEpilepsia: id
-        }
-    })
+    const ids = req.user.idUsuario
+    await baseDatosORM.tipoEpilepsia.destroy({ where: {idTipoEpilepsia: id } })
     req.flash('success', 'eliminacion')
     res.redirect('/tipoEpilepsia/lista/' + ids)
 }
